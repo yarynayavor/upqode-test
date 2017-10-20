1544ea9cd6b7056d7ee1d7c084601f8f1ef7ad8e
yarynayavor <yourhurt09@gmail.com>
2017-10-20 23:45:36 +0300

    added autoplay with different time and styling banner header and text

--- dev/js/script.js
+++ dev/js/script.js
@@ -19,3 +19,25 @@ $('ul.nav li.first-2').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+
+
+// different autoplay to sliders
+$(document).ready(function () {
+	var t;
+	var start = $('#myCarousel').find('.active').attr('data-interval');
+	t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
+
+	//console.log(start);
+	//console.log(t);
+
+	$('#myCarousel').on('slid.bs.carousel', function () {   
+	clearTimeout(t);  
+	var duration = $(this).find('.item.active').attr('data-interval');
+	console.log(duration);
+
+	$('#myCarousel').carousel('pause');
+	t = setTimeout("$('#myCarousel').carousel();", duration-1000);
+	//console.log(t);
+	});
+
+});
